7dc655627cd1ac1dd28a71ec3020cf6b70270037
samhalsall23 <samhalsall23@gmail.com>
2025-09-14 15:54:23 +1000

    feat(component services): add in quote button to services

--- src/components/OurServicesNew.tsx
+++ src/components/OurServicesNew.tsx
@@ -1,6 +1,7 @@
 import IconSquare, { IconSquareIconType } from "./icons/IconSquare";
 import { InViewSection } from "./InViewSection";
 import { SlashHeader } from "./SlashHeader";
+import { Button } from "./ui/button";
 
 const services = [
     {
@@ -69,10 +70,26 @@ export function OurServicesNew() {
 
             <div className="container mx-auto space-y-8 px-6 relative z-10 pt-20">
                 <InViewSection className="space-y-8 ">
-                    <SlashHeader text="WHAT WE DO" />
-                    <h2 className="h2-heading-text">
-                        Dedicated to offering a wide range of services
-                    </h2>
+                    <div className="flex justify-between items-start ">
+                        <div className="space-y-8 flex flex-col me-1">
+                            <SlashHeader text="WHAT WE DO" />
+                            <h2 className="h2-heading-text">
+                                Dedicated to offering a wide range of services
+                            </h2>
+                            <Button
+                                size={"md"}
+                                className="md:hidden mt-2 font-semibold rounded-full w-fit group hover:border-amber-400 hover:bg-amber-400"
+                                hasArrow={true}>
+                                Get a Quote
+                            </Button>
+                        </div>
+                        <Button
+                            size={"md"}
+                            className="hidden md:flex mt-6 font-semibold rounded-full group w-full sm:w-auto hover:border-amber-400 hover:bg-amber-400"
+                            hasArrow={true}>
+                            Get a Quote
+                        </Button>
+                    </div>
                 </InViewSection>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-10 pt-2">
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
 
 const buttonVariants = cva(
     "z-10 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -21,6 +22,7 @@ const buttonVariants = cva(
             size: {
                 default: "h-9 px-4 py-2",
                 sm: "h-8 rounded-md px-3 text-xs",
+                md: "h-8 xl:h-8 rounded-md px-4 xl:px-8 py-6 text-md lg:text-lg xl:text-xl",
                 lg: "h-8 xl:h-10 rounded-md px-6 xl:px-12 py-6 text-lg lg:text-xl xl:text-2xl",
                 icon: "h-9 w-9",
             },
@@ -36,17 +38,32 @@ export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
     asChild?: boolean;
+    hasArrow?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
+    (
+        {
+            className,
+            variant,
+            size,
+            asChild = false,
+            hasArrow = false,
+            ...props
+        },
+        ref
+    ) => {
         const Comp = asChild ? Slot : "button";
         return (
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
                 ref={ref}
-                {...props}
-            />
+                {...props}>
+                {props.children}
+                {hasArrow && (
+                    <ArrowRight className="ml-2 h-4 w-4 sm:h-5 sm:w-5 transition-transform duration-300 group-hover:translate-x-2" />
+                )}
+            </Comp>
         );
     }
 );
